3f46a893a40625a75cf5749e1835faa29e863e3d
Mikadmin <mikadmin@wow-attitude.com>
2012-05-22 11:04:32 +0200

    Zone dk
    Wrong Faction required
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ World/Quests/Eastern Kingdoms/Scarlet Enclave/00_fix_wrong_faction_requiered.sql
@@ -0,0 +1,6 @@
+-- Zone dk
+-- By mikadmin for Arkania
+-- Wrong Faction required
+ 
+UPDATE `quest_template` SET `RequiredRaces` = '0' WHERE `quest_template`.`entry` =12755;
+UPDATE `quest_template` SET `RequiredRaces` = '0' WHERE `quest_template`.`entry` =12756;
